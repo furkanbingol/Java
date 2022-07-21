d0a282f668ec06b143f0d5153cf812beef5cf9c1
Furkan <94539804+furkanbingol@users.noreply.github.com>
2022-07-21 19:39:39 +0300

    Create Professor.java

---
+++ 03-Classes&Objects/University03/Professor.java
@@ -0,0 +1,9 @@
+public class Professor {
+    String no;
+    String name;
+    String dob;  //dateOfBirth
+    String rank;
+    Department department;
+    Student[] advisee;
+    Course[] coursesGiven;
+}
