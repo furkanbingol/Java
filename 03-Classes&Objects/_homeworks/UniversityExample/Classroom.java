01c3c95c3658fafda152e104ce1b19e3a1a9b5f2
Furkan <94539804+furkanbingol@users.noreply.github.com>
2022-07-22 16:07:28 +0300

    Create Classroom.java

---
+++ 03-Classes&Objects/_homeworks/UniversityExample/Classroom.java
@@ -0,0 +1,20 @@
+public class Classroom {
+    String name;
+    Course[] courses;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Course[] getCourses() {
+        return courses;
+    }
+
+    public void setCourses(Course[] courses) {
+        this.courses = courses;
+    }
+}
