8e42263e61f3aabfc2f572e05d1d8ae7a14d1f00
Furkan <94539804+furkanbingol@users.noreply.github.com>
2022-07-23 19:48:20 +0300

    Create Point.java

---
+++ 03-Classes&Objects/_homeworks/Point/Point.java
@@ -0,0 +1,52 @@
+public class Point {
+    int x;
+    int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double distanceToOrigin(){
+        double distance;
+        distance = Math.sqrt(x*x + y*y);
+        return distance;
+    }
+
+    public double distanceToAnotherPoint(Point p){
+        double distance;
+        distance = Math.sqrt((x-p.x)*(x-p.x) + (y-p.y)*(y-p.y));
+        return distance;
+    }
+
+    public Point clone(){
+        Point newPoint = new Point();
+        newPoint.x = x;
+        newPoint.y = y;
+        return newPoint;
+    }
+
+    public void move(char coordinate, int distance){
+        if(coordinate == 'x'){
+            setX(x + distance);
+            System.out.println("New coordinates; x: " + getX() + ", y: " + getY());
+        }
+        else if(coordinate == 'y'){
+            setY(y + distance);
+            System.out.println("New coordinates; x: " + getX() + ", y: " + getY());
+        }
+        else
+            System.out.println("Error! Coordinate must be 'x' or 'y'");
+    }
+}
