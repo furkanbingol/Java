1770e5f2345d6cc2f2e4750750a7a79c882012a1
Furkan <94539804+furkanbingol@users.noreply.github.com>
2022-08-17 19:29:22 +0300

    Create Employee.java

---
+++ 07-Inheritance/factoryThree06/Employee.java
@@ -0,0 +1,32 @@
+package OOP.chapter5.factory3;
+
+public class Employee {
+    protected int no;
+    protected String name;
+    protected int year;
+    protected String department;
+
+    public static final int BASE_SALARY = 500;
+
+    public Employee(int no, String name, int year, String department){
+        this.no = no;
+        this.name = name;
+        this.year = year;
+        this.department = department;
+    }
+
+    public void work(){
+        System.out.println("Employee is working!");
+    }
+
+    public double calculateSalary(){
+        return year * BASE_SALARY;
+    }
+
+    public void printInfo(){
+        System.out.println("\nNo: " + no);
+        System.out.println("Name:" + name);
+        System.out.println("Year: " + year);
+        System.out.println("Department: " + department);
+    }
+}
