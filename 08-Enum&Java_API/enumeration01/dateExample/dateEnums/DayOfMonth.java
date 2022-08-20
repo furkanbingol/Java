6215ead61bfbbcbfb034864b3803732c6c11ccd7
Furkan <94539804+furkanbingol@users.noreply.github.com>
2022-08-20 22:08:02 +0300

    Create DayOfMonth.java

---
+++ 08-Enum&Java_API/enumeration01/dateExample/dateEnums/DayOfMonth.java
@@ -0,0 +1,9 @@
+package OOP.chapter6.enumeration.dateExample.dateEnums;
+
+public enum DayOfMonth {
+    DAY_1, DAY_2, DAY_3, DAY_4, DAY_5, DAY_6, DAY_7, DAY_8,
+    DAY_9, DAY_10, DAY_11, DAY_12, DAY_13, DAY_14, DAY_15,
+    DAY_16, DAY_17, DAY_18, DAY_19, DAY_20, DAY_21, DAY_22,
+    DAY_23, DAY_24, DAY_25, DAY_26, DAY_27, DAY_28, DAY_29,
+    DAY_30, DAY_31
+}
