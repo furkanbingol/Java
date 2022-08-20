eb53e8e83f470620fc7823fe7be71b179d0ecec4
Furkan <94539804+furkanbingol@users.noreply.github.com>
2022-08-20 22:08:34 +0300

    Create Year.java

---
+++ 08-Enum&Java_API/enumeration01/dateExample/dateEnums/Year.java
@@ -0,0 +1,7 @@
+package OOP.chapter6.enumeration.dateExample.dateEnums;
+
+public enum Year {
+    YEAR_2016, YEAR_2017, YEAR_2018, YEAR_2019,
+    YEAR_2020, YEAR_2021, YEAR_2022, YEAR_2023,
+    YEAR_2024, YEAR_2025, YEAR_2026, YEAR_2027
+}
