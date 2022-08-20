7488bf183814c9b86279d0f196b96b22beffb7aa
Furkan <94539804+furkanbingol@users.noreply.github.com>
2022-08-20 22:08:20 +0300

    Create Month.java

---
+++ 08-Enum&Java_API/enumeration01/dateExample/dateEnums/Month.java
@@ -0,0 +1,7 @@
+package OOP.chapter6.enumeration.dateExample.dateEnums;
+
+public enum Month {
+    JANUARY, FEBRUARY, MARCH, APRIL,
+    MAY, JUNE, JULY, AUGUST, SEPTEMBER,
+    OCTOBER, NOVEMBER, DECEMBER
+}
