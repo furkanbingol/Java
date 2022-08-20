56c90eb8d9fb749b3a6a1c814df3cc1ed3ce0386
Furkan <94539804+furkanbingol@users.noreply.github.com>
2022-08-20 22:09:12 +0300

    Create Date2.java

---
+++ 08-Enum&Java_API/enumeration01/dateExample/Date2.java
@@ -0,0 +1,13 @@
+package OOP.chapter6.enumeration.dateExample;
+
+public class Date2 {
+    private String dayOfMonth;
+    private String month;
+    private String year;
+
+    public Date2(String dayOfMonth, String month, String year) {
+        this.dayOfMonth = dayOfMonth;
+        this.month = month;
+        this.year = year;
+    }
+}
