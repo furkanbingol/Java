77605144bea9a9f0abaee0a700b8d0f22ea84a7f
Furkan <94539804+furkanbingol@users.noreply.github.com>
2022-08-20 22:09:42 +0300

    Create Date3.java

---
+++ 08-Enum&Java_API/enumeration01/dateExample/Date3.java
@@ -0,0 +1,14 @@
+package OOP.chapter6.enumeration.dateExample;
+
+public class Date3 {
+    //Use OOP.chapter6.enumeration.dateExample.constants.DateConstants for 'int fields'.
+    private int dayOfMonth;
+    private int month;
+    private int year;
+
+    public Date3(int dayOfMonth, int month, int year) {
+        this.dayOfMonth = dayOfMonth;
+        this.month = month;
+        this.year = year;
+    }
+}
