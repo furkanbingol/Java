8fa11e86653c9b20be5ef785a2505afa9e7907ef
Furkan <94539804+furkanbingol@users.noreply.github.com>
2022-10-04 19:24:20 +0300

    Add files via upload

---
+++ 10-Abstract_Classes/factory/Director.java
@@ -0,0 +1,46 @@
+package OOP.chapter8_abstractclasses.factory;
+
+public class Director extends Manager {
+    protected double bonus;
+
+    public Director(int no, String name, int year, String workingDepartment, String managingDepartment, double bonus) {
+        super(no, name, year, workingDepartment, managingDepartment);
+        this.bonus = bonus;
+    }
+
+    /* Bu metotu Director'un override etmesi zorunlu değil, edebilir etmeyedebilir. Çünkü Manager Class'ı zaten abstract
+       metot olan work() metotunu override etmişti.
+
+    @Override
+    public void work(){
+        System.out.println("Director is working!");
+        manage();
+    }
+    */
+
+    @Override
+    public void manage(){
+        System.out.println("Director manages whole company!");
+        makeAStrategicPlan();
+    }
+
+    public void makeAStrategicPlan(){
+        System.out.println("Director makes a strategic plan for the company!");
+    }
+
+    public double calculateSalary(){
+        return super.calculateSalary() + bonus;
+    }
+
+    public void printInfo(){
+        System.out.println("\nDirector Info");
+        super.printInfo();
+    }
+
+    @Override
+    public String toString() {
+        return "Director [bonus=" + bonus + ", departmentManaged=" + departmentManaged + ", no=" + no + ", name="
+                + name + ", year=" + year + ", department=" + department + "]";
+    }
+}
+
